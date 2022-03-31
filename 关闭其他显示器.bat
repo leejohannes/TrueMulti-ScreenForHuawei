fa61f6c9ea488a25bb4449535a5de9af89c6d277
leejohannes <leeyoo@leeyoo.org>
2022-03-31 11:34:43 +0800

    Update 关闭其他显示器.bat
    通过Get-WmiObject Win32_DesktopMonitor获得屏幕的PNPDeviceID速度更快些
    并且显示器按照标识顺序排列

--- 关闭其他显示器.bat
+++ 关闭其他显示器.bat
@@ -2,6 +2,6 @@
 CHCP 936>nul
 set /p a=1.关闭 2.开启
 if %a%==2 goto on
-powershell -c Disable-PnpDevice -InstanceId @((Get-PnpDevice -class Monitor).InstanceId ^| Select-Object -SkipLast 1)
+powershell -c Disable-PnpDevice -InstanceId ((Get-WmiObject Win32_DesktopMonitor).PNPDeviceID ^| Select-Object -Skip 1)
 :on
-powershell -c Enable-PnpDevice -InstanceId @((Get-PnpDevice -class Monitor).InstanceId ^| Select-Object -SkipLast 1)
+powershell -c Enable-PnpDevice -InstanceId ((Get-WmiObject Win32_DesktopMonitor).PNPDeviceID ^| Select-Object -Skip 1)

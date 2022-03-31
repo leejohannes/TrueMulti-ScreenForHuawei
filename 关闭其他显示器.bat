ebd0b0ade98509a0dd10a430de85e0b53051afff
leejohannes <leeyoo@leeyoo.org>
2022-03-31 11:43:32 +0800

    Update 关闭其他显示器.bat
    调整了脚本输入结构
    增加显示器关键参数显示
    增加了系统显示设置跳转
    增加亮度恢复

--- 关闭其他显示器.bat
+++ 关闭其他显示器.bat
@@ -1,7 +1,18 @@
 @echo off
 CHCP 936>nul
-set /p a=1.关闭 2.开启
-if %a%==2 goto on
+powershell -c Get-WmiObject Win32_DesktopMonitor ^| select DeviceID,Status,PNPDeviceID
 powershell -c Disable-PnpDevice -InstanceId ((Get-WmiObject Win32_DesktopMonitor).PNPDeviceID ^| Select-Object -Skip 1)
-:on
+echo.
+echo 可以连接多屏协同了
+echo 扩展模式还得去高级显示设置打开从桌面删除再关闭
+start ms-settings:display
+::还得加个“阻止windows使用此显示器”功能,目前没找到
+set a=n
+set /p a=是否恢复？y/n:
+if %a%==n goto END
 powershell -c Enable-PnpDevice -InstanceId ((Get-WmiObject Win32_DesktopMonitor).PNPDeviceID ^| Select-Object -Skip 1)
+:END
+powershell -c (Get-WmiObject -ns root/wmi -class wmiMonitorBrightNessMethods).WmiSetBrightness(5,((Get-WmiObject -Namespace root/WMI -ClassName WmiMonitorBrightness).CurrentBrightness+1))
+echo 亮度恢复中
+powershell -c (Get-WmiObject -ns root/wmi -class wmiMonitorBrightNessMethods).WmiSetBrightness(5,((Get-WmiObject -Namespace root/WMI -ClassName WmiMonitorBrightness).CurrentBrightness-1))
+pause
